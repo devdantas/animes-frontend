70011d0303f43871051ec8be2d383f075091b255
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-08-24 12:30:48 -0300

    adicionando template not found

--- src/components/global/404/NotFound.vue
+++ src/components/global/404/NotFound.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="notfound">
-    <h1>NotFound</h1>  
+    <div class="container">
+      <div class="row justify-content-center">
+        <img src="https://i.ibb.co/zJnq1TP/notfound-2.png" width="30%" height="500px">
+      </div>
+      <div class="row justify-content-center mt-2">
+        <h1>Página não encontrada</h1>
+        <h5>Erro 404</h5>
+      </div>
+      <div class="row justify-content-center">
+        <router-link to="/login">Voltar</router-link>
+      </div>      
+    </div>
   </div>  
 </template>
 
@@ -11,5 +22,4 @@ export default {
 </script>
 
 <style>
-
 </style>
