24c112ab0516ace4166614ccb3b2e306473b642d
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-08-27 23:00:36 -0300

    adicionando LayoutSwiper

---
+++ src/components/layout/LayoutSwiper.vue
@@ -0,0 +1,236 @@
+<template>
+  <div id="swiper">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div
+            data-swiper-parallax="-23%"
+            class="parallax-bg"
+            style="background-color: rgb(147, 165, 165); transform: translate3d(0%, 0px, 0px); transition-duration: 0ms;"
+          ></div>
+          <div class="container pt-5">
+            <div class="row">
+              <div class="col-lg-6">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  class="div-title"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <h1>Tate no Yuusha no Nariagari</h1>
+                </div>
+                <div
+                  data-swiper-parallax="-100"
+                  class="text"
+                  style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <p>
+                    Tate no Yuusha (apelidado de ‘ShieldBro’) acompanha um universitário que acaba indo parar em um
+                    mundo de
+                    fantasia, junto a outros estudantes. Cada um deles recebe uma arma lendária, sendo a do protagonista
+                    um
+                    escudo. Traído logo que chega lá, ele muda rapidamente sua atitude positiva para o ódio e desejo de
+                    vingança, até que encontra uma pequena escrava, que começa a ajudá-lo a mudar.
+                  </p>
+                  <p>
+                    Gêneros:
+                    <b>Ação, Aventura, Comédia, Drama, Fantasia, Romance, Seinen</b>
+                  </p>
+                  <p>
+                    <button
+                      data-swiper-parallax="-100"
+                      type="button"
+                      class="btn btn-md btn-primary"
+                    >Acessar</button>
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-6 pl-5 pb-5">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <img src="https://i.ibb.co/ZgBjrMR/tate-no-yuusha.png" class="responsive-img" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div
+            data-swiper-parallax="-23%"
+            class="parallax-bg"
+            style="background-color: rgb(239, 199, 18); transform: translate3d(0%, 0px, 0px); transition-duration: 0ms;"
+          ></div>
+          <div class="container pt-5">
+            <div class="row">
+              <div class="col-lg-6">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  class="div-title"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <h1>One Punch-man</h1>
+                </div>
+                <div
+                  data-swiper-parallax="-100"
+                  class="text"
+                  style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <p>
+                    Conta o dia-a-dia de um herói normal que está infeliz por ser forte o bastante para derrotar todos
+                    os seus oponentes com apenas um soco, já que isso não lhe traz mais a sensação de adrenalina ao
+                    enfrentar um inimigo poderoso.
+                  </p>
+                  <p>
+                    Gêneros:
+                    <b>Ação, Aventura, Comédia.</b>
+                  </p>
+                  <p>
+                    <button
+                      data-swiper-parallax="-100"
+                      type="button"
+                      class="btn btn-md btn-primary"
+                    >Acessar</button>
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-6 pl-5">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <img src="https://i.ibb.co/Kys09bg/one-p.png" class="responsive-img" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div
+            data-swiper-parallax="-23%"
+            class="parallax-bg"
+            style="background-color: rgb(33, 19, 12); transform: translate3d(0%, 0px, 0px); transition-duration: 0ms;"
+          ></div>
+          <div class="container pt-5">
+            <div class="row">
+              <div class="col-lg-6">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  class="div-title"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <h1>Black Clover</h1>
+                </div>
+                <div
+                  data-swiper-parallax="-100"
+                  class="text"
+                  style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <p>Em um mundo aonde todos nascem com poderes mágicos, Asta é a única exceção, enquanto seu amigo, Yuno, nasceu abençoado com grandes poderes mágicos. Quando completam 15 anos, os dois recebem itens que ampliam seus poderes, com Asta ganhando um raro Anti-Magic Grimoire, que permite a ele anular ou repelir magia. Agora ambos vão tentar realizar seu sonho em comum de se tornar um Wizard King.</p>
+                  <p>
+                    Gêneros:
+                    <b>Ação, Aventura, Comédia, Fantasia, Magia, Shounen.</b>
+                  </p>
+                  <p>
+                    <button
+                      data-swiper-parallax="-100"
+                      type="button"
+                      class="btn btn-md btn-primary"
+                    >Acessar</button>
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-6 pl-5">
+                <div
+                  data-swiper-parallax="-300"
+                  data-swiper-parallax-opacity="0"
+                  style="opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;"
+                >
+                  <img src="https://i.ibb.co/PCXQFG8/black-clover.png" class="responsive-img" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper/dist/js/swiper.esm.bundle";
+export default {
+  name: "swiper-show",
+  mounted() {
+    var swiper = new Swiper(".swiper-container", {
+      speed: 600,
+      autoplay: {
+        delay: 5000
+      },
+      parallax: true
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  background: #eee;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+
+.swiper-container {
+  width: 100%;
+  height: 75vh;
+}
+
+.swiper-slide {
+  font-size: 18px;
+  color: #fff;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+
+  .title {
+    font-size: 41px;
+    font-weight: 300;
+  }
+
+  .text {
+    font-size: 16px;
+    max-width: 450px;
+    line-height: 1.3;
+  }
+}
+
+.parallax-bg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 130%;
+  height: 100%;
+  -webkit-background-size: cover;
+  background-size: cover;
+  background-position: center;
+}
+
+.responsive-img {
+  width: 100%;
+  max-width: 420px;
+  height: auto;
+}
+</style>
